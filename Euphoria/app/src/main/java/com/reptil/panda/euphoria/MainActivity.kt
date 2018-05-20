d629dfa73be49ad09859035ebfeac4602070e2b6
pyroblaster <abartulovic@etfos.hr>
2018-05-21 00:23:01 +0200

    noAPI

---
+++ Euphoria/app/src/main/java/com/reptil/panda/euphoria/MainActivity.kt
@@ -0,0 +1,51 @@
+package com.reptil.panda.euphoria
+
+import android.support.v4.app.Fragment
+import android.os.Bundle
+import android.support.design.widget.BottomNavigationView
+import android.support.v7.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
+        when (item.itemId) {
+            R.id.navigation_home -> {
+                val homeFragment = HomeFragment.newInstance()
+                openFragment(homeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_portfolio -> {
+                val portfolioFragment = PortfolioFragment.newInstance()
+                openFragment(portfolioFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_trade -> {
+                val tradeFragment = TradeFragment.newInstance()
+                openFragment(tradeFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+            R.id.navigation_education -> {
+                val educationFragment = EducationFragment.newInstance()
+                openFragment(educationFragment)
+                return@OnNavigationItemSelectedListener true
+            }
+        }
+        false
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
+    }
+    private fun openFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.container, fragment)
+        transaction.addToBackStack(null)
+        transaction.commit()
+    }
+
+}
---
+++ Euphoria/app/src/main/java/com/reptil/panda/euphoria/PortfolioFragment.kt
@@ -0,0 +1,26 @@
+package com.reptil.panda.euphoria
+
+import android.content.Context
+import android.net.Uri
+import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class PortfolioFragment : Fragment() {
+
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
+                              savedInstanceState: Bundle?): View? {
+
+        return inflater.inflate(R.layout.fragment_portfolio, container, false)
+    }
+
+
+    companion object {
+        fun newInstance(): PortfolioFragment = PortfolioFragment()
+
+    }
+}
